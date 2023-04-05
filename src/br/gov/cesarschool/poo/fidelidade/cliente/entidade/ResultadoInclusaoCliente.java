87bd997b3aa144a4bffa9ec86352e9ce2bc51ee8
maluvsb <103540561+maluvsb@users.noreply.github.com>
2023-04-05 09:02:52 -0300

    Update ResultadoInclusaoCliente.java

--- src/br/gov/cesarschool/poo/fidelidade/cliente/entidade/ResultadoInclusaoCliente.java
+++ src/br/gov/cesarschool/poo/fidelidade/cliente/entidade/ResultadoInclusaoCliente.java
@@ -1,5 +1,20 @@
 package br.gov.cesarschool.poo.fidelidade.cliente.entidade;
 
 public class ResultadoInclusaoCliente {
+	private long numeroFidelidade;
+	private String mensagemErroValidacao;
 
+	public ResultadoInclusaoCliente(long numeroFidelidade, String mensagemErroValidacao) {
+		this.numeroFidelidade = numeroFidelidade;
+		this.mensagemErroValidacao = mensagemErroValidacao;
+	}
+	
+	public long getNumeroFidelidade() {
+		return numeroFidelidade;
+	}
+	
+	public String getMensagemErroValidacao() {
+		return mensagemErroValidacao;
+		
+	}
 }
